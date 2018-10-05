0f8db152f229e3eb09fada3df16442e43b89a507
Roger Perez <rperez@OneHealth.com>
2018-10-05 16:46:56 -0400

    Working on insert trains

--- public/assest/javascript/app.js
+++ public/assest/javascript/app.js
@@ -1,10 +1,6 @@
 var user = '';
-var logged = false;
 var provider = '';
-
-$("#toLogin").attr("hidden", false);
-$("#toLogout").attr("hidden", true);
-
+var logged = localStorage.getItem("logged");
 
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
@@ -15,11 +11,13 @@ function googleLogin() {
     firebase.auth().signInWithPopup(provider)
         .then(result => {
             user = result.user;
-            // document.write(`Hello ${user.displayName}`)
-            console.log(user);
-            logged = true;
+            localStorage.setItem("logged", true);
+            localStorage.setItem("user", user.displayName);
+
             $("#toLogin").attr("hidden", true);
             $("#toLogout").attr("hidden", false);
+            $("#name").text("Hello: " + user.displayName);
+            $("#newTrain").attr("hidden", false);
         })
         .catch(console.log);
 }
@@ -29,11 +27,13 @@ function githubLogin() {
     firebase.auth().signInWithPopup(provider)
         .then(result => {
             user = result.user;
-            // document.write(`Hello ${user.displayName}`)
-            console.log(user);
-            logged = true;
+            localStorage.setItem("logged", true);
+            localStorage.setItem("user", user.displayName);
+
             $("#toLogin").attr("hidden", true);
             $("#toLogout").attr("hidden", false);
+            $("#name").text("Hello: " + user.displayName);
+            $("#newTrain").attr("hidden", false);
         })
         .catch(console.log);
 }
@@ -42,10 +42,60 @@ function logout() {
     firebase.auth().signOut();
     $("#toLogin").attr("hidden", false);
     $("#toLogout").attr("hidden", true);
-    logged = false;
+    $("#name").empty();
+    $("#newTrain").attr("hidden", true);
+    localStorage.setItem("logged", false);
+    localStorage.setItem("user", "");
     user = '';
 }
 
+
 $(function () {
-    //console.log('Estoy listo')
+
+    if (!logged) {
+        $("#toLogout").attr("hidden", true);
+        $("#newTrain").attr("hidden", true);
+    } else {
+        if ( logged == "true" ) {
+            $("#toLogin").attr("hidden", true);
+            $("#newTrain").attr("hidden", false);
+        } else {
+            $("#toLogout").attr("hidden", true);
+            $("#newTrain").attr("hidden", true);
+        }
+    }
+
+    $("#name").text("Hello: " + localStorage.getItem("user"));
+    console.log("Today: " + moment().format("DD"));
+
+
+    const app = firebase.app();
+    const db = firebase.firestore();
+
+    db.collection("trains").onSnapshot(doc => {
+        $("#data").empty();
+        doc.forEach(function (doc) {
+            $("#data").append("<tr>")
+            $("#data").append("<td>" + doc.data().name + "</td>");
+            $("#data").append("<td>" + doc.data().destination + "</td>");
+            $("#data").append("<td>" + doc.data().frequency + "</td>");
+            $("#data").append("<td>data 2</td>");
+            $("#data").append("<td>data 3</td>");
+            $("#data").append("</tr>")
+        });
+    });
+
+
+    $("#newTrainSubmit").on("click", function(event) {
+        // prevent form from submitting
+        event.preventDefault();
+
+        var trainName = $("#inputName").val();
+        var trainDestination = $("#inputDestination").val();
+        var trainFrequency = $("#inputFrequency").val();
+
+        var newTrain = db.collection("trains").doc();
+        newTrain.set({'name': trainName, 'destination': trainDestination, 'frequency': trainFrequency});
+
+    });
 });

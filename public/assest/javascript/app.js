c712e9981ce6d41e3465dce34896e74f56d2231d
Roger Perez <rperezga@gmail.com>
2018-10-09 15:02:07 -0400

    Almost ready

--- public/assest/javascript/app.js
+++ public/assest/javascript/app.js
@@ -1,140 +1,178 @@
 var user = '';
 var provider = '';
 var logged = localStorage.getItem("logged");
-
-document.addEventListener("DOMContentLoaded", event => {
-    const app = firebase.app();
-});
-
-function googleLogin() {
-    provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-        .then(result => {
-            user = result.user;
-            localStorage.setItem("logged", true);
-            localStorage.setItem("user", user.displayName);
-
-            $("#toLogin").attr("hidden", true);
-            $("#toLogout").attr("hidden", false);
-            $("#name").text("Hello: " + user.displayName);
-            $("#newTrain").attr("hidden", false);
-        })
-        .catch(console.log);
-}
-
-function githubLogin() {
-    provider = new firebase.auth.GithubAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-        .then(result => {
-            user = result.user;
-            localStorage.setItem("logged", true);
-            localStorage.setItem("user", user.displayName);
-
-            $("#toLogin").attr("hidden", true);
-            $("#toLogout").attr("hidden", false);
-            $("#name").text("Hello: " + user.displayName);
-            $("#newTrain").attr("hidden", false);
-        })
-        .catch(console.log);
-}
-
-function logout() {
-    firebase.auth().signOut();
-    $("#toLogin").attr("hidden", false);
-    $("#toLogout").attr("hidden", true);
-    $("#name").empty();
-    $("#newTrain").attr("hidden", true);
-    localStorage.setItem("logged", false);
-    localStorage.setItem("user", "");
-    user = '';
-}
-
+var toEdit = '';
 
 $(function () {
 
     if (!logged) {
         $("#toLogout").attr("hidden", true);
         $("#newTrain").attr("hidden", true);
+        $("#trainLogin").attr("hidden", true);
     } else {
         if (logged == "true") {
             $("#toLogin").attr("hidden", true);
             $("#newTrain").attr("hidden", false);
+            $("#trainLogin").attr("hidden", false);
+            $("#trainLogout").attr("hidden", true);
         } else {
             $("#toLogout").attr("hidden", true);
             $("#newTrain").attr("hidden", true);
+            $("#trainLogin").attr("hidden", true);
+            $("#trainLogout").attr("hidden", false);
         }
     }
 
+    $("#googleLogin").on("click", function () {
+        provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(result => {
+                user = result.user;
+                localStorage.setItem("logged", true);
+                localStorage.setItem("user", user.displayName);
+
+                $("#toLogin").attr("hidden", true);
+                $("#toLogout").attr("hidden", false);
+                $("#name").text("Hello: " + user.displayName);
+                $("#newTrain").attr("hidden", false);
+
+                $("#trainLogin").attr("hidden", false);
+                $("#trainLogout").attr("hidden", true);
+            })
+            .catch(console.log);
+
+    })
+
+    $("#githubLogin").on("click", function () {
+        provider = new firebase.auth.GithubAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(result => {
+                user = result.user;
+                localStorage.setItem("logged", true);
+                localStorage.setItem("user", user.displayName);
+
+                $("#toLogin").attr("hidden", true);
+                $("#toLogout").attr("hidden", false);
+                $("#name").text("Hello: " + user.displayName);
+                $("#newTrain").attr("hidden", false);
+
+                $("#trainLogin").attr("hidden", false);
+                $("#trainLogout").attr("hidden", true);
+            })
+            .catch(console.log);
+    })
+
+    $("#logout").on("click", function () {
+        firebase.auth().signOut();
+        $("#toLogin").attr("hidden", false);
+        $("#toLogout").attr("hidden", true);
+        $("#name").empty();
+        $("#newTrain").attr("hidden", true);
+        localStorage.setItem("logged", false);
+        localStorage.setItem("user", "");
+        user = '';
+
+        $("#trainLogin").attr("hidden", true);
+        $("#trainLogout").attr("hidden", false);
+    })
+
     $("#name").text("Hello: " + localStorage.getItem("user"));
-    console.log("Today: " + moment().format("DD"));
 
-
-    const app = firebase.app();
     const db = firebase.firestore();
 
     db.collection("trains").onSnapshot(doc => {
-        $("#data").empty();
+        $("#dataLogin").empty();
+
         doc.forEach(function (doc) {
-            //console.log(doc.id)
+            const startTime = doc.data().startTime;
+            const next = moment(startTime, 'HH:mm').add(doc.data().frequency, 'minutes').format('HH:mm');
+            var duration = moment(next, 'HHmm').fromNow();
+            if (duration.slice(-3) == 'ago') {
+                const next = moment(startTime, 'HH:mm').add(doc.data().frequency, 'minutes').format('HH:mm');
+                var newTrain = db.collection("trains").doc(doc.id);
+                newTrain.set({ 'name': doc.data().name, 'destination': doc.data().destination, 'startTime': next, 'frequency': doc.data().frequency });
+            }
 
-            var row = $("<tr>");
-            row.addClass("train-data");
-            row.attr("id", doc.id);
+            if (logged == 'true') {
+                var rowLogin = $("<tr>");
+                rowLogin.addClass("train-data");
+                rowLogin.attr("id", doc.id);
 
-            var colName = $("<td>");
-            colName.append(doc.data().name);
+                var colName = $("<td>");
+                colName.append(doc.data().name);
 
-            var colDestination = $("<td>");
-            colDestination.append(doc.data().destination);
+                var colDestination = $("<td>");
+                colDestination.append(doc.data().destination);
 
-            var colFrequency = $("<td>");
-            colFrequency.append(doc.data().frequency);
+                var colFrequency = $("<td>");
+                colFrequency.append(doc.data().frequency);
 
-            var colNext = $("<td>");
-            colNext.append("data1");
+                var colNext = $("<td>");
+                const startTime = doc.data().startTime;
+                const next = moment(startTime, 'HH:mm').add(doc.data().frequency, 'minutes').format('HH:mm');
+                colNext.append(next);
 
-            var colAway = $("<td>");
-            colAway.append("data2");
+                var colAway = $("<td>");
+                var now = moment(new Date()).format("HHmm"); //todays date
+                var duration = moment(next, 'HHmm').from(moment(now, "HHmm"));
+                colAway.append(duration);
 
-            var colEdit = $("<td>");
-            colEdit.append("<i class='fa fa-edit'></i>");
+                var colEdit = $("<td id='edit'>");
+                colEdit.append("<i class='fa fa-edit'></i>");
 
-            var colDel = $("<td>");
-            colDel.append("<i class='fa fa-trash'></i>");
+                var colDel = $("<td id='delete'>");
+                colDel.append("<i class='fa fa-trash'></i>");
 
+                rowLogin.append(colName)
+                    .append(colDestination)
+                    .append(colFrequency)
+                    .append(colNext)
+                    .append(colAway)
+                    .append(colEdit)
+                    .append(colDel)
 
-            row.append(colName)
-                .append(colDestination)
-                .append(colFrequency)
-                .append(colNext)
-                .append(colAway)
-                .append(colEdit)
-                .append(colDel)
+                $("#dataLogin").append(rowLogin);
+            } else {
+                var rowLogout = $("<tr>");
+                rowLogout.addClass("train-data");
+                rowLogout.attr("id", doc.id);
 
-            $("#data").append(row);
+                var colName = $("<td>");
+                colName.append(doc.data().name);
 
+                var colDestination = $("<td>");
+                colDestination.append(doc.data().destination);
 
+                var colFrequency = $("<td>");
+                colFrequency.append(doc.data().frequency);
 
+                var colNext = $("<td>");
+                console.log(doc.data().startTime)
+                const startTime = doc.data().startTime;
+                const next = moment(startTime, 'HH:mm').add(doc.data().frequency, 'minutes').format('HH:mm');
+                colNext.append(next);
 
+                var colAway = $("<td>");
+                var now = moment(new Date()).format("HHmm"); //todays date
+                var duration = moment(next, 'HHmm').from(moment(now, "HHmm"));
+                colAway.append(duration);
 
-            // $("#data").append("<tr class='train-data' id='" + doc.id + "'>")
-            // $("#data").append("<td>" + doc.data().name + "</td>");
-            // $("#data").append("<td>" + doc.data().destination + "</td>");
-            // $("#data").append("<td>" + doc.data().frequency + "</td>");
-            // $("#data").append("<td>data 2</td>");
-            // $("#data").append("<td>data 3</td>");
-            // $("#data").append("<td> <i class='fa fa-edit'></i> </td>");
-            // $("#data").append("<td> <i class='fa fa-trash'></i> </td>");            
-            // $("#data").append("</tr>")
+                var colEdit = $("<td id='edit'>");
+                colEdit.append("<i class='fa fa-edit'></i>");
 
+                var colDel = $("<td id='delete'>");
+                colDel.append("<i class='fa fa-trash'></i>");
 
+                rowLogout.append(colName)
+                    .append(colDestination)
+                    .append(colFrequency)
+                    .append(colNext)
+                    .append(colAway)
+
+                $("#dataLogout").append(rowLogout);
+            }
         });
     });
-
-
-
-
-
 
     $("#newTrainSubmit").on("click", function (event) {
         // prevent form from submitting
@@ -145,13 +183,40 @@ $(function () {
         var trainStart = $("#inputStart").val();
         var trainFrequency = $("#inputFrequency").val();
 
-        var newTrain = db.collection("trains").doc();
-        newTrain.set({ 'name': trainName, 'destination': trainDestination, 'start-time': trainStart, 'frequency': trainFrequency });
-
-        $("#new-train")[0].reset();
+        if (toEdit == '') {
+            var newTrain = db.collection("trains").doc();
+            newTrain.set({ 'name': trainName, 'destination': trainDestination, 'startTime': trainStart, 'frequency': trainFrequency });
+            $("#new-train")[0].reset();
+        } else {
+            console.log(toEdit)
+            var newTrain = db.collection("trains").doc(toEdit);
+            newTrain.set({ 'name': trainName, 'destination': trainDestination, 'startTime': trainStart, 'frequency': trainFrequency });
+            $("#new-train")[0].reset();
+            $("#newTrainSubmit").text("Add Train");
+            toEdit = '';
+            alert('Train edited!');
+        }
     });
 
-    $(document).on("click", ".train-data", function () {
-        console.log($(this).attr("id"))
-    })
+    $(document).on("click", "#edit", function () {
+        toEdit = $(this).parent().attr("id");
+        var docum = db.collection("trains").doc($(this).parent().attr("id")).get().then(function (doc) {
+            $("#inputName").val(doc.data().name);
+            $("#inputDestination").val(doc.data().destination);
+            $("#inputStart").val(doc.data().startTime);
+            $("#inputFrequency").val(doc.data().frequency);
+            $("#newTrainSubmit").text("Update Train");
+        })
+    });
+
+    $(document).on("click", "#delete", function () {
+        if (confirm('Are you sure you want to delete the train: ')) {
+            db.collection("trains").doc($(this).parent().attr("id")).delete().then(function () {
+                alert('Train deleted!');
+            }).catch(function (error) {
+                console.error("Error removing document: ", error);
+            });
+        }
+    });
+
 });
